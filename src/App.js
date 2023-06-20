86d3a0000c109890a87453a7a072b1282e8b9eda
Mochamad Riza Syarif <rizasyarif20@gmail.com>
2023-06-20 15:16:31 +0700

    pdf preview
    Signed-off-by: Mochamad Riza Syarif <rizasyarif20@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { Viewer, Worker } from "@react-pdf-viewer/core";
 
-function App() {
+import "@react-pdf-viewer/core/lib/styles/index.css";
+
+
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js">
+      <div
+        style={{
+          height: "100%",
+          position: "relative",
+        }}
+      >
+        <Viewer
+          fileUrl={`${process.env.PUBLIC_URL}/bvt_pilars.pdf`}
+        />
+      </div>
+    </Worker>
   );
 }
-
-export default App;
