fc461eab305fb27d4992e48dbb39bcbe33da2906
Mochamad Riza Syarif <rizasyarif20@gmail.com>
2023-06-21 11:19:35 +0700

    change view pdf
    Signed-off-by: Mochamad Riza Syarif <rizasyarif20@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { Viewer, Worker } from "@react-pdf-viewer/core";
-
-import "@react-pdf-viewer/core/lib/styles/index.css";
-
-
+import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 
 export default function App() {
+  const defaultLayoutPluginInstance = defaultLayoutPlugin();
   return (
-    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.js">
+    <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.7.107/build/pdf.worker.js">
       <div
         className="bvt_wrapper"
       >
         <Viewer
           fileUrl={`${process.env.PUBLIC_URL}/bvt_pilars.pdf`}
+          plugins={[defaultLayoutPluginInstance]}
         />
       </div>
     </Worker>
